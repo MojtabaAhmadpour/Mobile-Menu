6a3c54145534d7f4637f951366510ca4c5bcd6e6
mr.rahmati <mr.rahmati.9105@gmail.com>
2023-09-27 22:37:51 +0330

    Placement of menu items

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 'use client'
 import { useState } from "react"
-import {TfiHome} from "react-icons/tfi"
-import {AiOutlineUser} from "react-icons/ai"
-import {BsHeart} from "react-icons/bs"
-import {PiAlarmLight} from "react-icons/pi"
+import { TfiHome } from "react-icons/tfi"
+import { AiOutlineUser } from "react-icons/ai"
+import { BsHeart } from "react-icons/bs"
+import { PiAlarmLight } from "react-icons/pi"
 
 
 export default function Home() {
@@ -17,20 +17,27 @@ export default function Home() {
     <>
 
       <div className="container">
-          <div className="menu">
-            <ul>
-              <li><a href="#"></a></li>
-              <li><a href="#"></a></li>
-              <li></li>
-              <li><a href="#"></a></li>
-              <li><a href="#"></a></li>
-
-            </ul>
-          </div>
-          <div className={`menuToggle ${isRotate ? "active" : ""}`}
+        <div className="menu">
+          <ul>
+            <li><a href="#">
+              <TfiHome />
+            </a></li>
+            <li><a href="#">
+              <AiOutlineUser />
+            </a></li>
+            <li></li>
+            <li><a href="#">
+              <BsHeart />
+            </a></li>
+            <li><a href="#">
+              <PiAlarmLight />
+            </a></li>
+          </ul>
+        </div>
+        <div className={`menuToggle ${isRotate ? "active" : ""}`}
           onClick={Change}>
-            <span></span>
-          </div>
+          <span></span>
+        </div>
       </div>
 
 
