e790186b07c7098400d734c52f72e73fe3a59b8d
mr.rahmati <mr.rahmati.9105@gmail.com>
2023-09-27 21:26:17 +0330

    Create a toggle menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,27 @@
+'use client'
+
+import { useState } from "react"
+
 export default function Home() {
+
+  const [isRotate, setIsRotate] = useState(false)
+  const Change = () => {
+    setIsRotate(!isRotate)
+  }
+
+
   return (
     <>
 
-      Hello Mr.rahmati
+      <div className="container">
+        <div classNames="navigation">
+          <div className={`menuToggle ${isRotate ? "active" : ""}`}
+          onClick={Change}
+
+          ></div>
+          <div className="menu"></div>
+        </div>
+      </div>
 
 
     </>
