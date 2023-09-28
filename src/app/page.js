405a96259703e66bfdc42332223ca445c02aa86d
mr.rahmati <mr.rahmati.9105@gmail.com>
2023-09-28 12:00:02 +0330

    end work

--- src/app/page.js
+++ src/app/page.js
@@ -4,39 +4,50 @@ import { TfiHome } from "react-icons/tfi"
 import { AiOutlineUser } from "react-icons/ai"
 import { BsHeart } from "react-icons/bs"
 import { PiAlarmLight } from "react-icons/pi"
+import Image from "next/image"
+import Ham from "../../public/Ham.svg"
+import Close from "../../public/Close.svg"
 
 
 export default function Home() {
 
   const [isRotate, setIsRotate] = useState(false)
+
   const Change = () => {
     setIsRotate(!isRotate)
   }
-
   return (
     <>
 
       <div className="container">
+        <div className={`menuToggle ${isRotate ? "active" : null}`}
+          onClick={Change}>
+
+          <div className="box">
+            {!isRotate ?
+              <Image src={Ham} alt="ham" />
+              :
+              <Image src={Close} alt="Close" />
+            }
+          </div>
+          <span></span>
+        </div>
         <div className="menu">
           <ul>
-            <li><a href="#">
+            <li style={{ "--i": "0.1s" }}><a href="#" title="">
               <TfiHome />
             </a></li>
-            <li><a href="#">
+            <li style={{ "--i": "0.2s" }}><a href="#">
               <AiOutlineUser />
             </a></li>
             <li></li>
-            <li><a href="#">
+            <li style={{ "--i": "0.2s" }}><a href="#">
               <BsHeart />
             </a></li>
-            <li><a href="#">
+            <li style={{ "--i": "0.1s" }}><a href="#">
               <PiAlarmLight />
             </a></li>
           </ul>
-        </div>
-        <div className={`menuToggle ${isRotate ? "active" : ""}`}
-          onClick={Change}>
-          <span></span>
         </div>
       </div>
 
